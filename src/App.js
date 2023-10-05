38456a83b754158f7577af92d646148731b6e11f
169398 <kulubiidris@gmail.com>
2023-10-06 01:25:36 +0300

    api key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// src/components/UrlShortener.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const UrlShortener = () => {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
+
+  const handleShorten = async () => {
+    try {
+      const response = await axios.post('5f47829b97fe4f17ab587976bd05bd3a', {
+        long_url: originalUrl,
+      });
+      setShortenedUrl(response.data.short_url);
+    } catch (error) {
+      console.error('Error shortening URL:', error);
+    }
+  };
+
+  //5f47829b97fe4f17ab587976bd05bd3a
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+        placeholder="Enter URL"
+      />
+      <button onClick={handleShorten}>Shorten URL</button>
+      {shortenedUrl && (
+        <div>
+          <p>Shortened URL: {shortenedUrl}</p>
+          <a href={shortenedUrl} target="_blank" rel="noopener noreferrer">
+            Open Shortened URL
+          </a>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default UrlShortener;
